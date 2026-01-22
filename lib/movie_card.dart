bc9014f0370072619362f1448480e17ed8892b24
CN-Liquid <adhikariaakash90@gmail.com>
2026-01-22 19:35:52 +0545

    Added a Card widget as a parent to the whole MovieCard widget

--- lib/movie_card.dart
+++ lib/movie_card.dart
@@ -12,34 +12,40 @@ class MovieCard extends StatelessWidget {
       onTap: () {
         Navigator.pushNamed(context, '/movie_page', arguments: movie);
       },
-      child: Container(
-        margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-        width: 275 * 2 / 3,
-        child: Column(
-          children: <Widget>[
-            Image.network(height: 275, movie.imageUrl),
-            Row(
+      child: Card(
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            width: 275 * 2 / 3,
+            child: Column(
               children: <Widget>[
-                Expanded(
-                  flex: 3,
-                  child: Text(
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    movie.title,
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    textAlign: TextAlign.end,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    '${movie.rating}',
-                  ),
+                Image.network(height: 275, movie.imageUrl),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: Text(
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        movie.title,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        textAlign: TextAlign.end,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        '${movie.rating}',
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
